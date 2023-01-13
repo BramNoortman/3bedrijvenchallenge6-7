452a647a1b394065a438ccedeb3077020da59309
BramNoortman <91888756+BramNoortman@users.noreply.github.com>
2023-01-13 14:37:32 +0000

    the big boi
    its the bigboi

---
+++ GameChallenge12/DemoGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using ExpressedEngine.ExpressedEngine;
+using System.Windows.Forms;
+
+namespace ExpressedEngine
+{
+    class DemoGame : ExpressedEngine.ExpressedEngine
+    {
+        Shape2D Player1;
+
+        bool left;
+        bool right;
+        bool up;
+        bool down;
+
+        public DemoGame() : base(new Vector2(815,715),"Expressed Engine Demo"){}
+
+        public override void Onload()
+        {
+            BackgroundColour = Color.Black;
+
+            
+
+            Player1 = new Shape2D(new Vector2(10, 10), new Vector2(20, 20), "Test");
+        }
+
+        public override void OnDraw()
+        {
+
+        }
+        
+        float x = 0.1f;
+        float y = 0.1f;
+        public override void OnUpdate()
+        {
+            if (up)
+            {
+                Player1.postition.Y -= 5f;
+            }
+            if (down)
+            {
+                Player1.postition.Y += 5f;
+            }
+            if (left)
+            {
+                Player1.postition.X -= 5f;
+            }
+            if (right)
+            {
+                Player1.postition.X += 5f;
+            }
+        }
+
+        
+
+        public override void GetKeyDown(KeyEventArgs e)
+        {
+           if (e.KeyCode == Keys.W) { up = true; }
+           if (e.KeyCode == Keys.S) { down = true; }
+           if (e.KeyCode == Keys.A) { left  = true; }
+           if (e.KeyCode == Keys.D) { right = true; }
+        }
+
+        public override void GetKeyUp(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W) { up = false; }
+            if (e.KeyCode == Keys.S) { down = false; }
+            if (e.KeyCode == Keys.A) { left = false; }
+            if (e.KeyCode == Keys.D) { right = false; }
+        }
+    }
+}
---
+++ GameChallenge12/ExpressedEngine/ExpressedEngine.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+    public abstract class ExpressedEngine
+    {
+        private Vector2 ScreenSize = new Vector2(512, 512);
+        private string Title = "New Game";
+        private Canvas Window = null;
+        private Thread GameLoopThread = null;
+
+        private static List<Shape2D> AllShapes = new List<Shape2D>();
+        private static List<Sprite2D> AllSprites = new List<Sprite2D>();
+
+        public Color BackgroundColour = Color.Beige;
+
+        public Vector2 CameraPosition = Vector2.Zero();
+        public float CameraAngle = 0f;
+        public ExpressedEngine(Vector2 ScreenSize, String Title)
+        {
+            Log.Info("Game Is Starting");
+            this.ScreenSize = ScreenSize;
+            this.Title = Title;
+
+            Window = new Canvas();
+            Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
+            Window.Text = this.Title;
+            Window.Paint += Renderer;
+            Window.KeyDown += Window_KeyDown;
+            Window.KeyUp += Window_KeyUp;
+            GameLoopThread = new Thread(GameLoop);
+            GameLoopThread.Start();
+
+            Application.Run(Window);
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            GetKeyUp(e);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            GetKeyDown(e);
+        }
+
+        public static void RegisterShape(Shape2D shape)
+        {
+            AllShapes.Add(shape);
+        }
+        public static void RegisterSprite(Sprite2D Sprite)
+        {
+            AllSprites.Add(Sprite);
+        }
+
+        public static void UnRegisterShape(Shape2D shape)
+        {
+            AllShapes.Remove(shape);
+        }
+        public static void UnRegisterSprite(Sprite2D Sprite)
+        {
+            AllSprites.Remove(Sprite);
+        }
+
+        void GameLoop()
+        {
+
+            Onload();
+            while (GameLoopThread.IsAlive) {
+
+                try
+                {
+                    OnDraw();
+                    Window.BeginInvoke((MethodInvoker)delegate { Window.Refresh(); });
+                    OnUpdate();
+                    Thread.Sleep(1);
+                }
+                catch 
+                {
+                    Log.Error("Game Has Not Been Found");
+                    
+                }
+            }
+        }
+
+        private void Renderer(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(BackgroundColour);
+
+            g.TranslateTransform(CameraPosition.X, CameraPosition.Y);
+            g.RotateTransform(CameraAngle);
+
+            foreach (Shape2D Shape in AllShapes)
+            {
+                g.FillEllipse(new SolidBrush(Color.SeaGreen),Shape.postition.X,Shape.postition.Y,Shape.Scale.X,Shape.Scale.Y);
+            }
+            foreach (Sprite2D Sprite in AllSprites)
+            {
+                g.DrawImage(Sprite.Sprite, Sprite.postition.X, Sprite.postition.Y, Sprite.Scale.X, Sprite.Scale.Y);
+            }
+
+
+
+        }
+
+        public abstract void Onload();
+        public abstract void OnUpdate();
+        public abstract void OnDraw();
+        public abstract void GetKeyDown(KeyEventArgs e);
+        public abstract void GetKeyUp(KeyEventArgs e);
+    }
+}
